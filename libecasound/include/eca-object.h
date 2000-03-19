1a99140100b52b289586afcdc5d8e61c88ca1c3c
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2000-03-19 02:57:04 +0000

    Added the missing libecasound/include/eca-object.h.

---
+++ libecasound/include/eca-object.h
@@ -0,0 +1,30 @@
+#ifndef _ECA_OBJECT_H
+#define _ECA_OBJECT_H
+
+#include <string>
+
+/**
+ * Virtual class for ecasound objects
+ *
+ * @author Kai Vehmanen
+ */
+class ECA_OBJECT {
+
+ public:
+
+  /**
+   * Object name used to identify the object type.
+   * Must be implemented in subclasses.
+   */
+  virtual string name(void) const = 0;
+
+  /**
+   * Object description. Description should be short, informative
+   * and unformatted.
+   */
+  virtual string description(void) const { return(""); }
+
+  virtual ~ECA_OBJECT (void) { }
+};
+
+#endif
