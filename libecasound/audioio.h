44a22e0766b5d5451c25653ca4dd5c2b744a6f76
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2000-10-08 17:12:19 +0000

    Changes to the class interface.

--- libecasound/audioio.h
+++ libecasound/audioio.h
@@ -11,12 +11,30 @@
 /**
  * Virtual base for all audio I/O classes (files, audio devices,
  * sound producing program modules, etc.)
+ *
+ * The class interface is divided into following sections:
+ *
+ *  - type definitions
+ *
+ *  - attributes
+ *
+ *  - configuration (setting and getting configuration parameters)
+ *
+ *  - functionality (control and runtime information)
+ *
+ *  - runtime information
+ *
+ *  - constructors and destructors
+ *
  * @author Kai Vehmanen
  */
 class AUDIO_IO : public DYNAMIC_OBJECT<string>,
 		 public ECA_AUDIO_POSITION {
 
  public:
+
+  // ===================================================================
+  // Type definitions
 
   /**
    * Input/Output mode
@@ -42,36 +60,90 @@ class AUDIO_IO : public DYNAMIC_OBJECT<string>,
    **/
   enum Io_mode { io_read = 1, io_write = 2, io_readwrite = 4 };
 
+  class SETUP_ERROR {
+    enum Type {
+      sample_format,    /* unsupported sample format */
+      channels,         /* unsupported channel count */
+      sample_rate,      /* unsupported sample_rate */
+      interleaving,     /* non-interleaved or interleaved channel organization not supported */
+      io_mode,          /* unsupported I/O mode */
+      buffersize,       /* unsupported buffersize */
+      blockmode,        /* non-blocking or blocking mode not supported */
+      dynamic_params,   /* invalid dynamic parameters (for instance invalid label()) */
+      unexpected        /* unexpected/unknown error */
+    };
+    
+   public:
+     const string& message(void) const;
+     Type type(void) const;
+     SETUP_ERROR(Type type, const string& message);
+
+   private:
+     Type type_rep;
+     string message_rep;
+  };
+
   // ===================================================================
-  // Buffering
+  // Attributes
+
+  virtual int supported_io_modes(void) const;
+  virtual bool supports_nonblocking_mode(void) const;
+  virtual bool supports_seeking(void) const;
+  virtual bool locked_audio_format(void) const;
+  
+  // ===================================================================
+  // Configuration (setting and getting configuration parameters)
 
   /**
-   * Set buffersize. Instead of specifying buffer length in seconds,
-   * parameters 'samples' and 'samples_rate' are used to determinate the 
-   * exact temporal length. When reading, this means that 'buffersize' of 
-   * sample data is read. When writing, 'buffersize' is only used for 
+   * Sets the sample buffer size in sample frames. Instead of specifying buffer 
+   * length in seconds, parameters 'samples' and 'sample_rate' are used to 
+   * determinate the length. When reading, this means that 'buffersize' 
+   * sample frames of data is read. When writing, 'buffersize' is only used for 
    * initializing devices and data structures. Device should be able 
-   * to write all sample data independent from current buffersize value.
-   * If audio object's sampling rate is changed, buffersize must also be adjusted.
+   * to write all sample data independently from current buffersize value.
+   * When audio object's sampling rate is changed, it's often wise to adjust
+   * the buffersize. Otherwise the real length of the buffer changes whenever
+   * sampling parameters are changed.
    */
   virtual void buffersize(long int samples, long int sample_rate) = 0;
 
   /**
-   * Returns buffersize in samples.
+   * Returns the buffersize in sample frames.
    */
   virtual long int buffersize(void) const = 0;
 
+  int io_mode(void) const;
+  const string& label(void) const;
+  string format_info(void) const;
+
+  void io_mode(int mode);
+  void label(const string& id_label);
+  void toggle_nonblocking_mode(bool value);
+
+  virtual string parameter_names(void) const { return("label"); }
+  virtual void set_parameter(int param, string value);
+  virtual string get_parameter(int param) const;
+
   // ===================================================================
-  // Basic input/output
+  // Functionality (control and runtime information)
+
+ public:
 
   /**
    * Read samples to buffer pointed by 'sbuf'. If necessary, the target 
    * buffer will be resized.
    *
+   * It's important to note that SAMPLE_BUFFER audio format cannot be
+   * changed during processing. This means that audio data must be converted
+   * from audio object's format to buffer object's format. SAMPLE_BUFFER 
+   * class provides tools for all normal conversion operations. If you need
+   * direct access to object's data, a lower abstaction level should be used
+   * (@see AUDIO_IO_DIRECT).
+   *
    * @see read_samples
    *
    * require:
-   *  io_mode() == si_read || io_mode() == si_readwrite
+   *  io_mode() == io_read || io_mode() == io_readwrite
    *  readable() == true
    *  sbuf != 0
    *
@@ -81,54 +153,35 @@ class AUDIO_IO : public DYNAMIC_OBJECT<string>,
   virtual void read_buffer(SAMPLE_BUFFER* sbuf) = 0;
 
   /**
-   * Write all data from sample buffer pointed by 'sbuf'.
+   * Write all data from sample buffer pointed by 'sbuf'. Notes
+   * concerning read_buffer() also apply to this routine.
    *
    * @see write samples
    *
    * require:
-   *  io_mode() == si_write || io_mode() == si_readwrite
+   *  io_mode() == io_write || io_mode() == io_readwrite
    *  writable() == true
    *  sbuf != 0
    */
   virtual void write_buffer(SAMPLE_BUFFER* sbuf) = 0;
 
   /**
-   * If supports_buffer_queries() == true, this function can be 
-   * used to check how many samples are available for reading, or 
-   * alternatively, how many samples can be written without 
-   * blocking. Effectively this is meant for implementing nonblocking 
-   * input and output with devices supporting it. Note, you 
-   * should use buffersize() call for setting how many bytes 
-   * read_buffer() will ask from the device.
-   *
-   * require:
-   *  supports_buffer_queries() == true
-   */
-  long int samples_available(void) const { return(0); }
-
-  /**
-   * Whether all data has been processed? If opened in mode 'si_read', 
-   * this means that end of stream has been reached. If opened in 
-   * 'si_write' or 'si_readwrite' modes, finished status usually
-   * means that an error has occured (no space left, etc).
-   */
-  virtual bool finished(void) const = 0;
-
-  // ===================================================================
-  // Audio object control
-
- public: 
-    
-  /**
-   * Open audio object (possibly in exclusive mode).
+   * Opens the audio object (possibly in exclusive mode).
    * This routine is meant for opening files and devices,
-   * loading libraries, etc. Object's audio format can be 
-   * adjusted during this call.
+   * loading libraries, etc. 
+   *
+   * At this point the various audio parameters are used
+   * for the first time. Unless locked_audio_format() is 'true', 
+   * object tries to use the audio format parameters set prior to 
+   * this call. If object doesn't support the given parameter
+   * combination, it can either try adjust them to closest
+   * matching, or in the worst case, throw an SETUP_ERROR 
+   * exception (see above).
    *
    * ensure:
    *  readable() == true || writable() == true
    */
-  virtual void open(void) = 0;
+  virtual void open(void) throw(SETUP_ERROR) = 0;
 
   /**
    * Close audio object. After calling this routine, 
@@ -142,49 +195,20 @@ class AUDIO_IO : public DYNAMIC_OBJECT<string>,
   virtual void close(void) = 0;
 
   // ===================================================================
-  // Parameter handling - see DYNAMIC_PARAMETERS documentation.
-  
-  virtual string parameter_names(void) const { return("label"); }
-  virtual void set_parameter(int param, string value);
-  virtual string get_parameter(int param) const;
-
-  // ===================================================================
-  // Status and info
+  // Runtime information
 
   /**
-   * Optional status string
+   * Effectively this is meant for implementing nonblocking 
+   * input and output with devices supporting it. If supports_nonblocking_mode() 
+   * == true, this function can be used to check how many samples are 
+   * available for reading, or alternatively, how many samples can be 
+   * written without blocking.  Note, you should use buffersize() call 
+   * for setting how many bytes read_buffer() will ask from the device.
+   *
+   * require:
+   *  supports_nonblocking_mode() == true
    */
-  virtual string status(void) const;
-
-  /**
-   * Returns info about supported I/O modes (bitwise-OR)
-   */
-  virtual int supported_io_modes(void) const { return(io_read | io_readwrite | io_write); }
-
-  /**
-   * Whether device supports the samples_available() function call.
-   */
-  virtual bool supports_buffer_queries(void) const { return(false); }
-
-  /**
-   * Whether audio format is locked
-   */
-  virtual bool locked_audio_format(void) const { return(false); }
-
-  /**
-   * Returns info about the current I/O mode.
-   */
-  int io_mode(void) const { return(si_mode_rep); }
-
-  /**
-   * The device name (usually set to device/file name).
-   */
-  const string& label(void) const { return(id_label_rep); }
-
-  /**
-   * Get a string containing info about sample format parameters.
-   */
-  string format_info(void) const;
+  virtual long int samples_available(void) const { return(0); }
 
   /**
    * Has device been opened (with open())?
@@ -192,65 +216,29 @@ class AUDIO_IO : public DYNAMIC_OBJECT<string>,
   bool is_open(void) const { return(open_rep); }
 
   /**
-   * If applicable, returns total length of the audio data stored
-   * into current audio object.
+   * Whether all data has been processed? If opened in mode 'io_read', 
+   * this means that end of stream has been reached. If opened in 
+   * 'io_write' or 'io_readwrite' modes, finished status usually
+   * means that an error has occured (no space left, etc). After 
+   * finished() has returned 'true', further calls to read_buffer() 
+   * and/or write_buffer() won't process any data.
    */
+  virtual bool finished(void) const = 0;
+
+  virtual bool nonblocking_mode(void) const;
+  virtual bool readable(void) const;
+  virtual bool writable(void) const;
+  virtual string status(void) const;
+
   ECA_AUDIO_TIME length(void) const;
-
-  /**
-   * Returns the current position.
-   */
   ECA_AUDIO_TIME position(void) const;
-
-  /**
-   * Is the audio object reading for reading? 
-   */
-  virtual bool readable(void) const { return(is_open() && io_mode() != io_write); }
-
-  /**
-   * Is the audio object reading for writing? 
-   */
-  virtual bool writable(void) const { return(is_open() && io_mode() != io_read); }
-
-  // --
-  // Format setup
-  // --
-
-  /**
-   * Set object input/output-mode. If the requested mode isn't
-   * supported, the nearest supported mode is used. Because 
-   * of this, it's wise to afterwards check whether the requested
-   * mode was accepted.
-   *
-   * require:
-   *  is_open() != true
-   */
-  void io_mode(int mode) { si_mode_rep = mode; }
-
-  /**
-   * Set object label
-   *
-   * require:
-   *  is_open() != true
-   */
-  void label(const string& id_label) { id_label_rep = id_label; }
 
  protected:
 
-  /**
-   * Sets the current position.
-   */
   void position(const ECA_AUDIO_TIME& v);
-
-  /**
-   * Sets the total length of audio object data.
-   */
   void length(const ECA_AUDIO_TIME& v);
 
-  /**
-   * Sets device's state to enabled or disabled.
-   */
-  void toggle_open_state(bool value) { open_rep = value; }
+  void toggle_open_state(bool value);
 
   // ===================================================================
   // Constructors and destructors
@@ -259,25 +247,17 @@ class AUDIO_IO : public DYNAMIC_OBJECT<string>,
 
   virtual AUDIO_IO* clone(void) = 0;
   virtual AUDIO_IO* new_expr(void) = 0;
-  virtual ~AUDIO_IO(void) { }
+  virtual ~AUDIO_IO(void);
   AUDIO_IO(const string& name = "unknown", 
 	   int mode = io_read, 
-	   const ECA_AUDIO_FORMAT& fmt = ECA_AUDIO_FORMAT())
-    : ECA_AUDIO_POSITION(fmt)
-    {
-      label(name);
-      io_mode(mode);
-      
-      position_in_samples(0);
-
-      readable_rep = writable_rep = open_rep = false;
-    }
+	   const ECA_AUDIO_FORMAT& fmt = ECA_AUDIO_FORMAT());
 
  private:
   
-  int si_mode_rep;
+  int io_mode_rep;
   string id_label_rep;
 
+  bool nonblocking_rep;
   bool readable_rep;
   bool writable_rep;
   bool open_rep;
