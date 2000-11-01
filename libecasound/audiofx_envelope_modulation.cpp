1de7d6ffaba9345ad503455d1575c6c77a3668fe
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2000-11-01 22:14:57 +0000

    New files

---
+++ libecasound/audiofx_envelope_modulation.cpp
@@ -0,0 +1,135 @@
+// ------------------------------------------------------------------------
+// audiofx_envelope-modulation.cpp: Effects which modify/modulate signal
+//                                  envelope
+// Copyright (C) 2000 Rob Coker, rcs@birch.net
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+// ------------------------------------------------------------------------
+
+#include <cmath>
+
+#include <kvutils/message_item.h>
+
+#include "samplebuffer_iterators.h"
+#include "audiofx_envelope_modulation.h"
+
+#include "eca-debug.h"
+#include "eca-error.h"
+
+EFFECT_PULSE_GATE::EFFECT_PULSE_GATE (parameter_type freq_Hz,
+				      parameter_type onTime_percent) {
+  set_parameter(1, freq_Hz);
+  set_parameter(2, onTime_percent);
+  currentTime = 0.0;
+}
+
+void EFFECT_PULSE_GATE::set_parameter(int param, parameter_type value) {
+  switch (param) {
+  case 1: 
+    if (value > 0)
+      {
+	period = 1.0/value; // seconds
+      }
+    else
+      {
+	MESSAGE_ITEM otemp;
+	otemp << "(audiofx_envelope_modulation) WARNING! Frequency must be greater than 0! ";
+	ecadebug->msg(otemp.to_string());
+      }
+    break;
+  case 2:
+    if ((value > 0) && (value < 100))
+      {
+	stopTime = (value/100.0)*period; // seconds
+      }
+    else
+      {
+	MESSAGE_ITEM otemp;
+	otemp << "(audiofx_envelope_modulation) WARNING! on time must be between 0 and 100 inclusive! ";
+	ecadebug->msg(otemp.to_string());
+      }
+    break;
+  }
+}
+
+CHAIN_OPERATOR::parameter_type EFFECT_PULSE_GATE::get_parameter(int param) const {
+  switch (param) {
+  case 1: 
+    return(1.0/period);
+    break;
+  case 2:
+    return (stopTime/period)*100.0;
+    break;
+  }
+  return(0.0);
+}
+
+void EFFECT_PULSE_GATE::init(SAMPLE_BUFFER* sbuf) { 
+  i.init(sbuf); 
+  set_samples_per_second(sbuf->sample_rate());
+  set_channels(sbuf->number_of_channels());
+  incrTime = 1.0/samples_per_second();
+}
+
+void EFFECT_PULSE_GATE::process(void) {
+  i.begin(); // iterate through all samples, one sample-frame at a
+             // time (interleaved)
+  while(!i.end()) {
+    currentTime += incrTime;
+    if (currentTime > period)
+      {
+	currentTime = 0.0;
+      }
+    if (currentTime > stopTime)
+      {
+	for(int n = 0; n < channels(); n++) {
+	  *i.current(n) = 0.0;
+	}
+      }
+    i.next();
+  }
+}
+
+EFFECT_PULSE_GATE_BPM::EFFECT_PULSE_GATE_BPM (parameter_type bpm,
+					      parameter_type ontime_percent) {
+  set_parameter(1, bpm);
+  set_parameter(2, ontime_percent);
+}
+
+void EFFECT_PULSE_GATE_BPM::set_parameter(int param, parameter_type value) {
+  switch (param) {
+  case 1: 
+    pulsegate_rep.set_parameter(1, value / 60.0);
+    break;
+  case 2:
+    pulsegate_rep.set_parameter(2, value);
+    break;
+  }
+}
+
+CHAIN_OPERATOR::parameter_type EFFECT_PULSE_GATE_BPM::get_parameter(int param) const {
+  switch (param) {
+  case 1: 
+    return (pulsegate_rep.get_parameter(1) * 60.0);
+    break;
+  case 2:
+    return (pulsegate_rep.get_parameter(2));
+    break;
+  }
+  return(0.0);
+}
+
+void EFFECT_PULSE_GATE_BPM::init(SAMPLE_BUFFER* sbuf) { pulsegate_rep.init(sbuf); }
+void EFFECT_PULSE_GATE_BPM::process(void) { pulsegate_rep.process(); }
---
+++ libecasound/audiofx_envelope_modulation.h
@@ -0,0 +1,75 @@
+#ifndef INCLUDED_AUDIOFX_ENVELOPE_MODULATION_H
+#define INCLUDED_AUDIOFX_ENVELOPE_MODULATION_H
+
+#include <vector>
+
+#include "samplebuffer_iterators.h"
+#include "audiofx.h"
+
+/**
+ * Virtual base for envelope modulation effects.
+ * @author Rob Coker
+ */
+class EFFECT_ENV_MOD : public EFFECT_BASE {
+
+ public:
+  virtual ~EFFECT_ENV_MOD(void) { }
+};
+
+/**
+ * Pulse shaped gate
+ * @author Rob Coker
+ */
+class EFFECT_PULSE_GATE: public EFFECT_ENV_MOD {
+
+  SAMPLE_ITERATOR_INTERLEAVED i;
+  parameter_type period;
+  parameter_type stopTime;
+  parameter_type currentTime;
+  parameter_type incrTime;
+
+ public:
+
+  virtual string name(void) const { return("Pulse Gate"); }
+  virtual string parameter_names(void) const  { return("freq-Hz,on-time-%"); }
+
+  virtual void set_parameter(int param, parameter_type value);
+  virtual parameter_type get_parameter(int param) const;
+
+  virtual void init(SAMPLE_BUFFER *insample);
+  virtual void process(void);
+
+  EFFECT_PULSE_GATE (parameter_type freq_Hz = 1.0, parameter_type onTime_percent = 50.0);
+  virtual ~EFFECT_PULSE_GATE(void) { }
+  EFFECT_PULSE_GATE* clone(void)  { return new EFFECT_PULSE_GATE(*this); }
+  EFFECT_PULSE_GATE* new_expr(void)  { return new EFFECT_PULSE_GATE(); }
+};
+
+/**
+ * Wrapper class for pulse shaped gate providing 
+ * a beats-per-minute (bpm) based parameters.
+ *
+ * @author Kai Vehmanen
+ */
+class EFFECT_PULSE_GATE_BPM : public EFFECT_ENV_MOD {
+
+  EFFECT_PULSE_GATE pulsegate_rep;
+
+ public:
+
+  virtual string name(void) const { return("Pulse gate BPM"); }
+  virtual string parameter_names(void) const  { return("bpm,on-time-msec"); }
+
+  virtual void set_parameter(int param, parameter_type value);
+  virtual parameter_type get_parameter(int param) const;
+
+  virtual void init(SAMPLE_BUFFER *insample);
+  virtual void process(void);
+
+  EFFECT_PULSE_GATE_BPM (parameter_type bpm = 120.0, parameter_type ontime_percent = 5.0);
+  virtual ~EFFECT_PULSE_GATE_BPM(void) { }
+  EFFECT_PULSE_GATE_BPM* clone(void)  { return new EFFECT_PULSE_GATE_BPM(*this); }
+  EFFECT_PULSE_GATE_BPM* new_expr(void)  { return new EFFECT_PULSE_GATE_BPM(); }
+};
+
+#endif
