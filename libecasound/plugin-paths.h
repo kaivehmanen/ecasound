2d9ffc4278599ce21a9bcbbc2a907a5ce873cf49
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2001-08-18 21:46:56 +0000

    NEWS

--- libecasound/plugin-paths.h
+++ libecasound/plugin-paths.h
@@ -2,8 +2,8 @@
 #define INCLUDED_PLUGIN_PATHS_H
 
 /* NOTE: changed by Junichi Uekawa for Debian-compliance */
-const char* ecasound_plugin_path = "/usr/local/lib/ecasound-plugins";
-const char* ecasound_resource_path = "/usr/local/share/ecasound";
-const char* ecasound_ladspa_path = "/usr/local/lib/ladspa";
+const char* ecasound_plugin_path = "/usr/local/eca/lib/ecasound-plugins";
+const char* ecasound_resource_path = "/usr/local/eca/share/ecasound";
+const char* ecasound_ladspa_path = "/usr/local/eca/lib/ladspa";
 
 #endif
