6e85853f36c999de1ab5b9b5789c988a6bcec988
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2001-10-21 07:38:31 +0000

    Removed file.

--- libecasound/plugin-paths.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCLUDED_PLUGIN_PATHS_H
-#define INCLUDED_PLUGIN_PATHS_H
-
-/* NOTE: changed by Junichi Uekawa for Debian-compliance */
-const char* ecasound_plugin_path = "/usr/local/eca/lib/ecasound-plugins";
-const char* ecasound_resource_path = "/usr/local/eca/share/ecasound";
-const char* ecasound_ladspa_path = "/usr/local/eca/lib/ladspa";
-
-#endif
