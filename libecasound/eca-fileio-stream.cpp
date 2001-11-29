5a0ff849fe4a3e1d9d2b60c68d11f3dd05173f3c
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2001-11-29 22:15:39 +0000

    Namespace bugs fixes and .cdr update.

--- libecasound/eca-fileio-stream.cpp
+++ libecasound/eca-fileio-stream.cpp
@@ -18,7 +18,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 // ------------------------------------------------------------------------
 
-#include <stdio.h>
+#include <cstdio>
 #include <cstring>
 #include <errno.h>
 #include <unistd.h>
