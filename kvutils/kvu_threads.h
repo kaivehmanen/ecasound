03106fa94a52ee61c268227ca30b3c7dd7ff20f2
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2002-04-28 19:36:20 +0000

    Missing std:: scope specifier added.

--- kvutils/kvu_threads.h
+++ kvutils/kvu_threads.h
@@ -6,6 +6,6 @@
 
 int kvu_pthread_mutex_spinlock (pthread_mutex_t *mp, long int spinlimit);
 int kvu_pthread_timed_wait(pthread_mutex_t* mutex, pthread_cond_t* cond, long int seconds);
-string kvu_pthread_timed_wait_result(int result, const std::string& prefix);
+std::string kvu_pthread_timed_wait_result(int result, const std::string& prefix);
 
 #endif
