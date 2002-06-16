3edb57fea88e1325ca94212676cf05bc5ab054bd
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2002-06-16 22:13:14 +0000

    Typo fixes. More verbose MIDI debug messages.

--- libecasound/midi-cc.cpp
+++ libecasound/midi-cc.cpp
@@ -43,7 +43,7 @@ CONTROLLER_SOURCE::parameter_t MIDI_CONTROLLER::value(void)
   }
   if (server() != 0)
     value_rep =
-      static_cast<double>(server()->last_controller_value(channel_rep, controller));
+      static_cast<double>(server()->last_controller_value(channel_rep, controller_rep));
       
   value_rep /= 127.0;
   return(value_rep);
@@ -63,7 +63,7 @@ void MIDI_CONTROLLER::init(void)
     MESSAGE_ITEM otemp;
     otemp << "(midi-cc) MIDI-controller initialized using controller ";
     otemp.setprecision(0);
-    otemp << controller << " and channel " << channel << ".";
+    otemp << controller_rep << " and channel " << channel_rep << ".";
     ECA_LOG_MSG(ECA_LOGGER::user_objects, otemp.to_string());
 }
 
@@ -74,16 +74,16 @@ void MIDI_CONTROLLER::set_parameter(int param, CONTROLLER_SOURCE::parameter_t va
     controller_rep = static_cast<int>(value);
     if (controller_rep < 0 ||
 	controller_rep > 127) {
-      contorller_rep = 1;
+      controller_rep = 1;
       ECA_LOG_MSG(ECA_LOGGER::info, 
 		  "(midi-cc) Controller number must be a number between 0 and 127. Defaulting to controller 0");
     }
     break;
   case 2: 
     channel_rep = static_cast<int>(value);
-    if (channel < 1 ||
-	channel > 16) {
-      channel = 1;
+    if (channel_rep < 1 ||
+	channel_rep > 16) {
+      channel_rep = 1;
       ECA_LOG_MSG(ECA_LOGGER::info, 
 		  "(midi-cc) MIDI-channel must be a number between 1 and 16. Defaulting to channel 1.");
     }
--- libecasound/midi-server.cpp
+++ libecasound/midi-server.cpp
@@ -1,6 +1,6 @@
 // ------------------------------------------------------------------------
 // midi-server.cpp: MIDI i/o engine serving generic clients.
-// Copyright (C) 2001 Kai Vehmanen (kaiv@wakkanet.fi)
+// Copyright (C) 2001-2002 Kai Vehmanen (kai.vehmanen@wakkanet.fi)
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -42,7 +42,8 @@ const unsigned int MIDI_SERVER::max_queue_size_rep = 32768;
 /**
  * Helper function for starting the slave thread.
  */
-void* start_midi_server_io_thread(void *ptr) {
+void* start_midi_server_io_thread(void *ptr)
+{
   sigset_t sigset;
   sigemptyset(&sigset);
   sigaddset(&sigset, SIGINT);
@@ -57,7 +58,8 @@ void* start_midi_server_io_thread(void *ptr) {
 /**
  * Slave thread.
  */
-void MIDI_SERVER::io_thread(void) { 
+void MIDI_SERVER::io_thread(void)
+{
   fd_set fds;
   unsigned char buf[3];
   struct timeval tv;
@@ -85,6 +87,8 @@ void MIDI_SERVER::io_thread(void) {
     int read_bytes = 0;
     if (retval) {
       if (FD_ISSET(fd, &fds) == true) {
+	// FIXME:: should use MIDI_IO::write_bytes() instead,
+	//         poll_descriptor is only meant for poll() and select()
 	read_bytes = ::read(fd, buf, 1);
       }
     }
@@ -95,7 +99,7 @@ void MIDI_SERVER::io_thread(void) {
       break;
     }
     else {
-//        cerr << "(midi-server) read bytes: " << read_bytes << endl;
+      // cerr << "(midi-server) read bytes: " << read_bytes << endl;
       for(int n = 0; n < read_bytes; n++) {
 	buffer_rep.push_back(buf[n]);
 	while(buffer_rep.size() > max_queue_size_rep) {
@@ -121,7 +125,8 @@ void MIDI_SERVER::io_thread(void) {
 /**
  * Constructor.
  */
-MIDI_SERVER::MIDI_SERVER (void) { 
+MIDI_SERVER::MIDI_SERVER (void)
+{
   running_status_rep = 0;
   current_ctrl_channel_rep = -1;
   current_ctrl_number = -1;
@@ -134,7 +139,8 @@ MIDI_SERVER::MIDI_SERVER (void) {
 /**
  * Destructor. Doesn't delete any client objects.
  */
-MIDI_SERVER::~MIDI_SERVER(void) { 
+MIDI_SERVER::~MIDI_SERVER(void)
+{
   if (is_enabled() == true) disable();
 }
 
@@ -144,7 +150,8 @@ MIDI_SERVER::~MIDI_SERVER(void) {
  * ensure
  *  is_running() == true
  */
-void MIDI_SERVER::start(void) { 
+void MIDI_SERVER::start(void)
+{
   stop_request_rep.set(0);
   running_rep.set(1);
   ECA_LOG_MSG(ECA_LOGGER::user_objects, "(midi-server) starting processing");
@@ -161,7 +168,8 @@ void MIDI_SERVER::start(void) {
  * initializes the stop procedure. Processing will
  * stop once the i/o-thread acknowledges the stop request.
  */
-void MIDI_SERVER::stop(void) { 
+void MIDI_SERVER::stop(void)
+{
   stop_request_rep.set(1);
   ECA_LOG_MSG(ECA_LOGGER::user_objects, "(midi-server) stopping processing");
   send_mmc_stop();
@@ -172,7 +180,8 @@ void MIDI_SERVER::stop(void) {
  * Initializes the MIDI-server by resetting 
  * all MIDI-related state info.
  */
-void MIDI_SERVER::init(void) {
+void MIDI_SERVER::init(void)
+{
   running_status_rep = 0;
   current_ctrl_channel_rep = -1;
   current_ctrl_number = -1;
@@ -184,7 +193,8 @@ void MIDI_SERVER::init(void) {
  * ensure:
  *  is_enabled() == true
  */
-void MIDI_SERVER::enable(void) {
+void MIDI_SERVER::enable(void)
+{
   init();
   running_rep.set(0);
   stop_request_rep.set(0);
@@ -234,7 +244,8 @@ bool MIDI_SERVER::is_enabled(void) const { return(thread_running_rep); }
  *  is_running() != true
  *  is_enabled() != true
  */
-void MIDI_SERVER::disable(void) {
+void MIDI_SERVER::disable(void)
+{
   // --------
   DBC_REQUIRE(is_enabled() == true);
   // --------
@@ -256,7 +267,8 @@ void MIDI_SERVER::disable(void) {
 /**
  * Whether the MIDI server has been started?
  */
-bool MIDI_SERVER::is_running(void) const { 
+bool MIDI_SERVER::is_running(void) const
+{
   if (running_rep.get() == 0) return(false); 
   return(true);
 }
@@ -265,7 +277,8 @@ bool MIDI_SERVER::is_running(void) const {
  * Registers a new client object. Midi server doesn't
  * handle initializing and opening of client objects.
  */
-void MIDI_SERVER::register_client(MIDI_IO* mobject) { 
+void MIDI_SERVER::register_client(MIDI_IO* mobject)
+{
   clients_rep.push_back(mobject);
   ECA_LOG_MSG(ECA_LOGGER::user_objects, 
 		"Registering client " +
@@ -277,7 +290,8 @@ void MIDI_SERVER::register_client(MIDI_IO* mobject) {
  * Unregisters the client object given as the argument. No
  * resources are freed during this call.
  */
-void MIDI_SERVER::unregister_client(MIDI_IO* mobject) { 
+void MIDI_SERVER::unregister_client(MIDI_IO* mobject)
+{
   for(unsigned int n = 0; n < clients_rep.size(); n++) {
     if (clients_rep[n] == mobject) {
       clients_rep[n] = 0;
@@ -290,7 +304,8 @@ void MIDI_SERVER::unregister_client(MIDI_IO* mobject) {
  * Registers a new MIDI-handler. The server will send 
  * all received MIDI-data to the handler.
  */
-void MIDI_SERVER::register_handler(MIDI_HANDLER* object) { 
+void MIDI_SERVER::register_handler(MIDI_HANDLER* object)
+{
   handlers_rep.push_back(object);
   ECA_LOG_MSG(ECA_LOGGER::user_objects, 
 		"Registering handler " +
@@ -302,7 +317,8 @@ void MIDI_SERVER::register_handler(MIDI_HANDLER* object) {
  * Unregisters the handler object given as the argument. No
  * resources are freed during this call.
  */
-void MIDI_SERVER::unregister_handler(MIDI_HANDLER* object) { 
+void MIDI_SERVER::unregister_handler(MIDI_HANDLER* object)
+{ 
   for(unsigned int n = 0; n < handlers_rep.size(); n++) {
     if (handlers_rep[n] == object) {
       handlers_rep[n] = 0;
@@ -316,16 +332,18 @@ void MIDI_SERVER::unregister_handler(MIDI_HANDLER* object) {
  * during processing. 
  *
  * Note! Id '127' is specified as the all-device 
- *       id-numberin the MMC-spec.
+ *       id-number in the MMC-spec.
  */
-void MIDI_SERVER::add_mmc_send_id(int id) {
+void MIDI_SERVER::add_mmc_send_id(int id)
+{
   mmc_send_ids_rep.push_back(id);
 }
 
 /**
  * Removes a MMC-message client.
  */
-void MIDI_SERVER::remove_mmc_send_id(int id) {
+void MIDI_SERVER::remove_mmc_send_id(int id)
+{
   mmc_send_ids_rep.remove(id);
 }
 
@@ -357,7 +375,8 @@ void MIDI_SERVER::send_midi_bytes(int dev_id, unsigned char* buf, int bytes) {
 /**
  * Sends an MMC-command to all MMC-send client device ids.
  */
-void MIDI_SERVER::send_mmc_command(unsigned int cmd) {
+void MIDI_SERVER::send_mmc_command(unsigned int cmd)
+{
   unsigned char buf[6];
   buf[0] = 0xf0;
   buf[1] = 0x7f;
@@ -367,6 +386,10 @@ void MIDI_SERVER::send_mmc_command(unsigned int cmd) {
   buf[5] = 0xf7;
   std::list<int>::const_iterator p = mmc_send_ids_rep.begin();
   while(p != mmc_send_ids_rep.end()) {
+    ECA_LOG_MSG(ECA_LOGGER::system_objects, 
+		"(midi-server) Sending MMC message " + 
+		kvu_numtostr(cmd) + " to device-id " +
+		kvu_numtostr(*p) + ".");
     buf[2] = static_cast<unsigned char>(*p);
     send_midi_bytes(1, buf, 6);
     ++p;
@@ -376,34 +399,55 @@ void MIDI_SERVER::send_mmc_command(unsigned int cmd) {
 /**
  * Sends MMC-start to all MMC-send client device ids.
  */
-void MIDI_SERVER::send_mmc_start(void) { send_mmc_command(0x02); }
+void MIDI_SERVER::send_mmc_start(void)
+{ 
+  send_mmc_command(0x02); 
+}
 
 /**
  * Sends MMC-stop to all MMC-send client device ids.
  */
-void MIDI_SERVER::send_mmc_stop(void) { send_mmc_command(0x01); }
+void MIDI_SERVER::send_mmc_stop(void)
+{ 
+  send_mmc_command(0x01);
+}
 
 /**
  * Sends a MIDI-start message.
  */
-void MIDI_SERVER::send_midi_start(void) { 
+void MIDI_SERVER::send_midi_start(void)
+{ 
   unsigned char byte[1] = { 0xfa };
+
+  ECA_LOG_MSG(ECA_LOGGER::system_objects, 
+	      "(midi-server) Sending MIDI-start message.");
+
   send_midi_bytes(1, byte, 1);
 }
 
 /**
  * Sends a MIDI-continue message.
  */
-void MIDI_SERVER::send_midi_continue(void) { 
+void MIDI_SERVER::send_midi_continue(void)
+{
   unsigned char byte[1] = { 0xfb };
+
+  ECA_LOG_MSG(ECA_LOGGER::system_objects, 
+	      "(midi-server) Sending MIDI-continue message.");
+
   send_midi_bytes(1, byte, 1);
 }
 
 /**
  * Sends a MIDI-stop message.
  */
-void MIDI_SERVER::send_midi_stop(void) { 
+void MIDI_SERVER::send_midi_stop(void)
+{
   unsigned char byte[1] = { 0xfc };
+
+  ECA_LOG_MSG(ECA_LOGGER::system_objects, 
+	      "(midi-server) Sending MIDI-stop message.");
+
   send_midi_bytes(1, byte, 1);
 }
 
@@ -411,7 +455,8 @@ void MIDI_SERVER::send_midi_stop(void) {
  * Requests that server will follow the latest value of 
  * controller 'ctrl' on channel 'channel'.
  */
-void MIDI_SERVER::add_controller_trace(int channel, int ctrl) {
+void MIDI_SERVER::add_controller_trace(int channel, int ctrl)
+{
   controller_values_rep[std::pair<int,int>(channel,ctrl)] = 0;
 }
 
@@ -419,7 +464,8 @@ void MIDI_SERVER::add_controller_trace(int channel, int ctrl) {
  * Requests that server stops following the latest value of
  * controller 'ctrl' on channel 'channel'.
  */
-void MIDI_SERVER::remove_controller_trace(int channel, int controller) {
+void MIDI_SERVER::remove_controller_trace(int channel, int controller)
+{
   std::map<std::pair<int,int>,int>::iterator p = controller_values_rep.find(std::pair<int,int>(channel,controller));
   if (p != controller_values_rep.end()) {
     controller_values_rep.erase(p);
@@ -430,7 +476,8 @@ void MIDI_SERVER::remove_controller_trace(int channel, int controller) {
  * Returns the latest traced value of controller 'ctrl' on 
  * channel 'channel'.
  */
-int MIDI_SERVER::last_controller_value(int channel, int ctrl) const {  
+int MIDI_SERVER::last_controller_value(int channel, int ctrl) const
+{
   std::map<std::pair<int,int>,int>::iterator p = controller_values_rep.find(std::pair<int,int>(channel,ctrl));
   if (p != controller_values_rep.end()) {
     return(controller_values_rep[std::pair<int,int>(channel,ctrl)]);
@@ -441,8 +488,8 @@ int MIDI_SERVER::last_controller_value(int channel, int ctrl) const {
 /**
  * Parses the received MIDI date.
  */
-void MIDI_SERVER::parse_receive_queue(void) {
-
+void MIDI_SERVER::parse_receive_queue(void)
+{
   while(buffer_rep.size() > 0) {
     unsigned char byte = buffer_rep.front();
     buffer_rep.pop_front();
@@ -471,20 +518,20 @@ void MIDI_SERVER::parse_receive_queue(void) {
 	if (current_ctrl_channel_rep != -1) {
 	  if (current_ctrl_number == -1) {
 	    current_ctrl_number = static_cast<int>(byte);
-//        	    cerr << endl << "C:" << current_ctrl_number << ".";
+	    // cerr << endl << "C:" << current_ctrl_number << ".";
 	  }
 	  else {
 	    if (controller_values_rep.find(std::pair<int,int>(current_ctrl_channel_rep,current_ctrl_number)) 
 		!= controller_values_rep.end()) {
 	      controller_values_rep[std::pair<int,int>(current_ctrl_channel_rep,current_ctrl_number)] = static_cast<int>(byte);
-//      	      cerr << endl << "(midi-server) Value:" 
-//      		   << controller_values_rep[pair<int,int>(current_ctrl_channel_rep,current_ctrl_number)] 
-//      		   << ", ch:" << current_ctrl_channel_rep 
-//      		   << ", ctrl:" << current_ctrl_number << ".";
+	      // cerr << endl << "(midi-server) Value:" 
+	      //      << controller_values_rep[pair<int,int>(current_ctrl_channel_rep,current_ctrl_number)] 
+	      //      << ", ch:" << current_ctrl_channel_rep 
+	      //      << ", ctrl:" << current_ctrl_number << ".";
 	    }
-//  	    else {
-//  	      cerr << endl << "E:" << " found an entry we are not following..." << endl;
-//  	    }
+	    // else {
+	    //   cerr << endl << "E:" << " found an entry we are not following..." << endl;
+	    // }
 	    current_ctrl_number = -1;
 	  }
 	}
