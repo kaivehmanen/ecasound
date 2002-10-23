1d8228a0d74796e3c3d1646dd425de871a074100
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2002-10-23 21:15:12 +0000

    Changed libecasoundc license to LGPL.

--- libecasoundc/ecasoundc.h
+++ libecasoundc/ecasoundc.h
@@ -1,6 +1,28 @@
 #ifndef INCLUDED_ECASOUNDC_H
 #define INCLUDED_ECASOUNDC_H
 
+/** ------------------------------------------------------------------------
+ * ecasoundc.h: Standalone C implementation of the 
+ *              ecasound control interface
+ * Copyright (C) 2000-2002 Kai Vehmanen (kai.vehmanen@wakkanet.fi)
+ * Copyright (C) 2001 Aymeric Jeanneau (ajeanneau@cvf.fr)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * -------------------------------------------------------------------------
+ */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
--- libecasoundc/ecasoundc_sa.c
+++ libecasoundc/ecasoundc_sa.c
@@ -3,12 +3,9 @@
  *                        ecasound control interface
  */
 
-/* FIXME: change to LGPL...? */
 /* FIXME: add check for big sync-error -> ecasound probably 
  *        died so better to give an error */
 /* FIXME: add check for msgsize errors */
-/* FIXME: parsing the content of an empty string list returns
- *        the wrong value (-> rettype str instead of content str) */
 /* FIXME: get rid of the static string lengths (C... blaah ;)) */
 /* FIXME: add documentation for ECASOUND envvar */
 /* FIXME: add proper signal handling */
@@ -19,19 +16,19 @@
  * Copyright (C) 2000-2002 Kai Vehmanen (kai.vehmanen@wakkanet.fi)
  * Copyright (C) 2001 Aymeric Jeanneau (ajeanneau@cvf.fr)
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * -------------------------------------------------------------------------
  * History: 
