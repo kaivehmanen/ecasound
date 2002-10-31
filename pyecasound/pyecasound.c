406bd778a01c65af36d79294a24ca046f2da2701
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2002-10-31 16:03:56 +0000

    Updated the license to LGPL.

--- pyecasound/pyecasound.c
+++ pyecasound/pyecasound.c
@@ -6,11 +6,11 @@
 // pyecasound.cpp: Python interface to the ecasound control interface
 // Copyright (C) 2000-2002 Kai Vehmanen (kai.vehmanen@wakkanet.fi)
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
