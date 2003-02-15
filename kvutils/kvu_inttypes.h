74d74bbcdd4154ba3371c3ec01bd1fcfd93f7b74
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2003-02-15 17:45:57 +0000

    Use of CYGWIN32 deprecated, now using CYGWIN instead.

--- kvutils/kvu_inttypes.h
+++ kvutils/kvu_inttypes.h
@@ -38,7 +38,7 @@
 #elif HAVE_SYS_TYPES_H
 #include <sys/types.h>
 /* Cygwin32 doesn't define all types */
-#ifdef __CYGWIN32__
+#ifdef __CYGWIN__
 typedef u_int8_t uint8_t;
 typedef u_int16_t uint16_t;
 typedef u_int32_t uint32_t;
