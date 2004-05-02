d90af8d57456914071f37d7d79993e62b6e4d034
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2004-05-02 20:42:42 +0000

    Updated rubyecasound, Jan/2004.

--- rubyecasound/ecasound.rb
+++ rubyecasound/ecasound.rb
@@ -1,3 +1,20 @@
+# This is a native implementation of Ecasound's control interface for Ruby.
+# Copyright (C) 2003 - 2004  Jan Weil <jan.weil@web.de>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# ---------------------------------------------------------------------------
 =begin
 = ruby-ecasound
 
@@ -11,25 +28,10 @@ ecasound-response = eci.command("iam-command-here")
 TODO:
 Is there a chance that the ecasound process gets zombified?
 
-= copyright
-Copyright (C) 2003 Jan Weil <Jan.Weil@web.de>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 =end
 
 require "timeout"
+require "thread"
 
 class File
     def self::which(prog, path=ENV['PATH'])
@@ -71,14 +73,21 @@ end # VersionString
 module Ecasound
 
 REQUIRED_VERSION = VersionString.new("2.2.0")
-TIMEOUT = 5 # seconds before sync is called 'lost'
+TIMEOUT = 15 # seconds before sync is called 'lost'
 
 class EcasoundError < RuntimeError; end
+class EcasoundCommandError < EcasoundError
+    attr_accessor :command, :error
+    def initialize(command, error)
+        @command = command
+        @error = error
+    end
+end
 
 class ControlInterface
     @@ecasound = ENV['ECASOUND'] || File::which("ecasound")
     
-    if not File::executable?(@@ecasound)
+    if not File::executable?(@@ecasound.to_s)
         raise("ecasound executable not found")
     else
         @@version = VersionString.new(`#{@@ecasound} --version`.split("\n")[0][/\d\.\d\.\d/])
@@ -87,63 +96,67 @@ class ControlInterface
         end
     end
     
-    def initialize(args = "")
+    def initialize(args = nil)
+        @mutex = Mutex.new()
         @ecapipe = IO.popen("-", "r+") # fork!
         
         if @ecapipe.nil?
             # child
-            # stderr has to be redirected to avoid buffering problems 
             $stderr.reopen(open("/dev/null", "w"))
-            exec("#{@@ecasound} -c -D -d:256 " + args)
+            exec("#{@@ecasound} #{args.to_s} -c -D -d:256 ")
         else
+            @ecapipe.sync = true
             # parent
             command("int-output-mode-wellformed")
         end
     end
 
-    def command(cmd)
-        cmd.strip!()
-        @ecapipe.write(cmd + "\n")
+    def cleanup()
+        @ecapipe.close()
+    end
 
-        response = ""
-        
-        # TimeoutError is raised unless response is complete
-        begin
-            timeout(TIMEOUT) do
-                loop do
-                    response += read()
-                    if response =~ /256 ([0-9]{1,5}) (\-|i|li|f|s|S|e)\r\n(.*)\r\n\r\n/m
-                        # we have a valid response
-                        break
+    def command(cmd)
+        @mutex.synchronize do
+            cmd.strip!()
+            #puts "command: #{cmd}"
+            
+            @ecapipe.write(cmd + "\n")
+
+            response = ""
+            begin
+                # TimeoutError is raised unless response is complete
+                timeout(TIMEOUT) do
+                    loop do
+                        response += read()
+                        break if response =~ /256 ([0-9]{1,5}) (\-|i|li|f|s|S|e)\r\n(.*)\r\n\r\n/m
                     end
                 end
+            rescue TimeoutError
+                raise(EcasoundError, "lost synchronisation to ecasound subprocess\nlast command was: '#{cmd}'")
             end
-        rescue TimeoutError
-            raise(EcasoundError, "lost synchronisation to ecasound subprocess")
-        end
-        
-        content = $~[3][0, $~[1].to_i()]
+            
+            content = $3[0, $1.to_i()]
 
-        #print "command: #{cmd}\n"
-        #print "type: '#{$~[2]}'\n"
-        #print "length: #{$~[1]}\n"
-        #print "content: #{content}\n"
+            #puts "type: '#{$2}'"
+            #puts "length: #{$1}"
+            #puts "content: #{content}"
 
-        case $~[2]
-            when "e"
-                raise(EcasoundError, "command '#{cmd}' failed: #{content}")
-            when "-"
-                return nil
-            when "s"
-                return content
-            when "S"
-                return content.split(",")
-            when "f"
-                return content.to_f()
-            when "i", "li"
-                return content.to_i()
-            else
-                raise(EcasoundError, "parsing of ecasound's output produced an unknown return type")
+            case $2
+                when "e"
+                    raise(EcasoundCommandError.new(cmd, content))
+                when "-"
+                    return nil
+                when "s"
+                    return content
+                when "S"
+                    return content.split(",")
+                when "f"
+                    return content.to_f()
+                when "i", "li"
+                    return content.to_i()
+                else
+                    raise(EcasoundError, "parsing of ecasound's output produced an unknown return type")
+            end
         end
     end
 
@@ -152,7 +165,7 @@ class ControlInterface
     def read()
         buffer = ""
         while select([@ecapipe], nil, nil, 0)
-            buffer = buffer + @ecapipe.read(1)
+            buffer += @ecapipe.read(1) || ""
         end
         return buffer
     end
