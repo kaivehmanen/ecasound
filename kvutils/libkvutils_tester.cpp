eb16d549e54f6428607bee85f6d3b603380779df
Kai Vehmanen <kaiv@.devtag.AUTHORS>
2004-12-14 02:11:58 +0000

    New optimized value queue implementation, VALUE_QUEUE_RT_C, added to kvutils.

--- kvutils/libkvutils_tester.cpp
+++ kvutils/libkvutils_tester.cpp
@@ -23,12 +23,18 @@
 #include <string>
 
 #include <cstdio>
-#include <stdio.h> /* for AIX */
-#include <time.h> /* ANSI-C: clock() */
+#include <stddef.h>  /* ANSI-C: size_t */
+#include <stdio.h>   /* for AIX */
+#include <stdlib.h>  /* ANSI-C: atoi() */
+#include <time.h>    /* ANSI-C: clock() */
+
+#include <unistd.h>  /* POSIX: sleep() */
 
 #include "kvu_locks.h"
 #include "kvu_numtostr.h"
+#include "kvu_rtcaps.h"
 #include "kvu_utils.h"
+#include "kvu_value_queue.h"
 
 using namespace std;
 
@@ -43,14 +49,15 @@ using namespace std;
  */
 
 #ifdef VERBOSE
-#define ECA_TEST_ENTRY()   do { printf("\n%s:%d - Test started", __FILE__, __LINE__); } while(0)
-#define ECA_TEST_SUCCESS() do { printf("\n%s:%d - Test passed\n", __FILE__, __LINE__); return 0; } while(0)
+#define ECA_TEST_ENTRY()   do { printf("\n%s:%d - Test started\n", __FILE__, __LINE__); } while(0)
+#define ECA_TEST_SUCCESS() do { printf("%s:%d - Test passed\n", __FILE__, __LINE__); return 0; } while(0)
 #define ECA_TEST_FAIL(x,y) do { printf("\n%s:%d - Test failed: \"%s\"\n", __FILE__, __LINE__, y); return x; } while(0)
-#define ECA_TEST_CASE()    do { printf("."); fflush(stdout); } while(0)
+#define ECA_TEST_NOTE(x)   do { printf("%s:%d - %s\n", __FILE__, __LINE__, x); fflush(stdout); } while(0)
 #else
 #define ECA_TEST_ENTRY()   ((void) 0)
 #define ECA_TEST_SUCCESS() return 0
 #define ECA_TEST_FAIL(x,y) return x
+#define ECA_TEST_NOTE(x)   ((void) 0)
 #endif
 
 /* --------------------------------------------------------------------- 
@@ -66,12 +73,15 @@ typedef int (*kvu_test_t)(void);
 static int kvu_test_1(void);
 static int kvu_test_2(void);
 static int kvu_test_3(void);
+static int kvu_test_4(void);
 static void* kvu_test_1_helper(void* ptr);
+static void* kvu_test_4_helper(void* ptr);
 
 static kvu_test_t kvu_funcs[] = { 
   kvu_test_1, 
   kvu_test_2, 
   kvu_test_3, 
+  kvu_test_4, 
   NULL 
 };
 
@@ -82,11 +92,23 @@ static kvu_test_t kvu_funcs[] = {
 int main(int argc, char *argv[])
 {
   int n, failed = 0;
-  
-  for(n = 0; kvu_funcs[n] != NULL; n++) {
-    int ret = kvu_funcs[n]();
-    if (ret != 0) {
-      ++failed;
+
+  if (argc > 1) {
+    /* run just a single test */
+    size_t m = atoi(argv[1]);
+    if (m > 0 && m < (sizeof(kvu_funcs) / sizeof(kvu_test_t))) {
+      if (kvu_funcs[m - 1]() != 0) {
+	++failed;
+      }
+    }
+  }
+  else {
+    /* run all tests */
+    for(n = 0; kvu_funcs[n] != NULL; n++) {
+      int ret = kvu_funcs[n]();
+      if (ret != 0) {
+	++failed;
+      }
     }
   }
 
@@ -230,7 +252,7 @@ static int kvu_test_2(void)
   }
 
   if (kvu_get_argument_number(5, argument) != "arg5") {
-    fprintf(stderr, "vec2: '%s'.\n", kvu_get_argument_number(5, argument).c_str());
+    // fprintf(stderr, "vec2: '%s'.\n", kvu_get_argument_number(5, argument).c_str());
     ECA_TEST_FAIL(1, "kvu_test_2 kvu_get_argument_number (1)"); 
   }
 
@@ -295,4 +317,93 @@ static int kvu_test_3(void)
   }
 
   ECA_TEST_SUCCESS();
+}
+
+static const int kvu_test_4_iterations_const = 16384;
+static int kvu_test_4_retval = 0;
+
+/**
+ * Tests the VALUE_QUEUE_RT_C class implementation.
+ */
+static int kvu_test_4(void)
+{
+  ECA_TEST_ENTRY();
+
+  /* guarantee bounded execution time only upto 16 items */
+  VALUE_QUEUE_RT_C rqueue (16);
+
+  ECA_TEST_NOTE("start-test");
+
+  pthread_t thread;
+  pthread_create(&thread, NULL, kvu_test_4_helper, (void*)&rqueue);
+
+  sleep(1);
+
+  ECA_TEST_NOTE("start-item-push");
+
+  for(int iter = 0; iter < kvu_test_4_iterations_const; iter++) {
+    // fprintf(stderr, "%s:%d push.\n", __FUNCTION__, __LINE__);
+    rqueue.push_back(iter, 1.0f);
+    // sleep(1);
+  }
+
+  int *res = 0;
+  pthread_join(thread, (void**)&res);
+  if (*res != 0) {
+    ECA_TEST_FAIL(1, "kvu_test_4 slave-thread-failed"); 
+  }
+
+  ECA_TEST_NOTE("end-test.");
+
+  ECA_TEST_SUCCESS();
+}
+
+/**
+ * The real-time consumer thread used in testing VALUE_QUEUE_RT_C.
+ */
+static void* kvu_test_4_helper(void* ptr)
+{
+  VALUE_QUEUE_RT_C *rqueue = (VALUE_QUEUE_RT_C*)ptr;
+  kvu_test_4_retval = 0;
+
+  ECA_TEST_NOTE("start-thread.");
+
+  int res = kvu_set_thread_scheduling(SCHED_FIFO, 1);
+  if (res == 0) {
+    ECA_TEST_NOTE("schedfifo-scheduling-enabled");
+  }
+  else {
+    ECA_TEST_NOTE("schedfifo-scheduling-disabled");
+  }
+
+  int last_received_v = 0;
+  for(int received = 0; received < kvu_test_4_iterations_const; ) {
+    if (rqueue->is_empty() != true) {
+      const pair<int, double>* ref = rqueue->front();
+      if (ref != rqueue->invalid_item()) {
+	// fprintf(stderr, "%s:%d front-success.\n", __FUNCTION__, __LINE__);
+	++received;
+	last_received_v = ref->first + 1;
+	if (received != last_received_v) {
+	  ECA_TEST_NOTE("queue-sync-error"); 
+	  kvu_test_4_retval = -1;
+	}
+	rqueue->pop_front();
+      }
+      else {
+	ECA_TEST_NOTE("corner-case-queue-busy");
+      }
+    }
+  }
+
+  if (last_received_v != kvu_test_4_iterations_const) {
+    ECA_TEST_NOTE("end-of-queue-sync-error"); 
+    kvu_test_4_retval = -1;
+  }
+
+  ECA_TEST_NOTE("exit-thread");
+
+  pthread_exit(&kvu_test_4_retval);
+  /* never reached */
+  return 0;
 }
