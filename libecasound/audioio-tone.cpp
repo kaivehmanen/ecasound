e3073f7a3b1c40d901992b373323d18594326618
Kai Vehmanen <kvehmanen -at- eca -dot- cx>
2008-03-09 19:14:43 +0200

    Added a new tone generator audio object type.

--- libecasound/audioio-tone.cpp
+++ libecasound/audioio-tone.cpp
@@ -3,7 +3,7 @@
 //
 // Adaptation to Ecasound:
 // 
-// Copyright (C) 2007 Kai Vehmanen (adaptation to Ecasound)
+// Copyright (C) 2007,2008 Kai Vehmanen (adaptation to Ecasound)
 //
 // Sources for sine generation (cmt-src-1.15/src/sine.cpp):
 //
@@ -33,7 +33,9 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <cmath>
+#include <cstdlib>
+
+#include <math.h> /* C++'s standard <cmath> does not define M_PI */
 
 #include <kvu_message_item.h>
 #include <kvu_numtostr.h>
@@ -41,14 +43,24 @@
 
 #include "eca-object-factory.h"
 #include "samplebuffer.h"
-#include "sample-specs.h"
 #include "audioio-tone.h"
 
 #include "eca-error.h"
 #include "eca-logger.h"
 
+/**
+ * FIXME notes  (last update 2008-03-06)
+ *
+ *  - define the syntax: is this 'tone', 'sinetone', 
+ *    'tone=sine', ..., or?
+ *  - add support for multichannel testing (different
+ *    frequnecies for different channels?)
+ */
+
 using std::cout;
 using std::endl;
+using std::atof;
+using std::string;
 
 /* Sine table size is given by (1 << SINE_TABLE_BITS). */
 #define SINE_TABLE_BITS 14
@@ -60,11 +72,11 @@ SAMPLE_SPECS::sample_t g_fPhaseStepBase = 0;
 static void initialise_sine_wavetable(void)
 {
   if (g_pfSineTable == NULL) {
-    long lTableSize = (1 << SINE_TABLE_BITS);
+    unsigned long lTableSize = (1 << SINE_TABLE_BITS);
     double dShift = (double(M_PI) * 2) / lTableSize;
     g_pfSineTable = new SAMPLE_SPECS::sample_t[lTableSize];
     if (g_pfSineTable != NULL)
-      for (long lIndex = 0; lIndex < lTableSize; lIndex++)
+      for (unsigned long lIndex = 0; lIndex < lTableSize; lIndex++)
 	g_pfSineTable[lIndex] = SAMPLE_SPECS::sample_t(sin(dShift * lIndex));
   }
   if (g_fPhaseStepBase == 0) {
@@ -72,7 +84,11 @@ static void initialise_sine_wavetable(void)
   }
 }
 
-AUDIO_IO_TONE::AUDIO_IO_TONE (const std::string& name)
+AUDIO_IO_TONE::AUDIO_IO_TONE (const std::string& name) 
+  :  m_lPhaseStep(0), 
+     m_fCachedFrequency(0),
+     m_fLimitFrequency(0),
+     m_fPhaseStepScalar(0)
 {
   set_label(name);
   initialise_sine_wavetable();
@@ -85,33 +101,181 @@ AUDIO_IO_TONE::~AUDIO_IO_TONE(void)
 AUDIO_IO_TONE* AUDIO_IO_TONE::clone(void) const
 {
   AUDIO_IO_TONE* target = new AUDIO_IO_TONE();
+
   for(int n = 0; n < number_of_params(); n++) {
     target->set_parameter(n + 1, get_parameter(n + 1));
   }
+
+  target->set_position_in_samples(position_in_samples());
+  if (ECA_AUDIO_POSITION::length_set())
+    target->ECA_AUDIO_POSITION::set_length_in_samples(ECA_AUDIO_POSITION::length_in_samples());
+
+  target->buffersize_rep = buffersize_rep;
+  target->finished_rep = finished_rep;
+  target->m_lPhase = m_lPhase;
+  target->m_lPhaseStep = m_lPhaseStep;
+  DBC_CHECK(target->m_fCachedFrequency == m_fCachedFrequency);
+  target->m_fLimitFrequency = m_fLimitFrequency;
+  target->m_fPhaseStepScalar = m_fPhaseStepScalar;
+
   return target;
 }
 
 void AUDIO_IO_TONE::open(void) throw(AUDIO_IO::SETUP_ERROR &)
 {
+  DBC_CHECK(samples_per_second() != 0);
+
+  if (io_mode() != AUDIO_IO::io_read)
+    throw(SETUP_ERROR(SETUP_ERROR::io_mode, "AUDIO_IO_TONE: Writing to tone generator not allowed!"));
+
+  finished_rep = false;
+  m_fLimitFrequency
+    = SAMPLE_SPECS::sample_t(samples_per_second() * 0.5);
+  m_fPhaseStepScalar
+    = SAMPLE_SPECS::sample_t(g_fPhaseStepBase / samples_per_second());
+
+  /* recalculate m_fLimitFrequency and mfPhaseStepScalar */
+  if (m_fCachedFrequency) 
+    setPhaseStepFromFrequency(m_fCachedFrequency, true);
+
+  AUDIO_IO::open();
 }
 
 void AUDIO_IO_TONE::close(void)
 {
+  AUDIO_IO::close();
 }
 
+bool AUDIO_IO_TONE::finite_length_stream(void) const
+{
+  if (ECA_AUDIO_POSITION::length_set() != true)
+    return true;
+  else 
+    return false;
+}
 
 void AUDIO_IO_TONE::read_buffer(SAMPLE_BUFFER* sbuf)
 {
   /* write to sbuf->buffer[ch], similarly as the LADSPA
    * chainops */
+
+  /* FIXME: actually, we should reset the channel count
+   *        every time -> we output an 'sbuf' with fixed
+   *        number of channels */
+  sbuf->number_of_channels(channels());
+
+  /* set the length according to our buffersize */
+  if (ECA_AUDIO_POSITION::length_set() &&
+      (position_in_samples() + buffersize()) 
+      >= ECA_AUDIO_POSITION::length_in_samples()) {
+    /* over requested duration, adjust buffersize */
+    sbuf->length_in_samples(ECA_AUDIO_POSITION::length_in_samples() 
+			    - position_in_samples());
+    finished_rep = true;
+  }
+  else
+    sbuf->length_in_samples(buffersize());
+  
+  i.init(sbuf);
+  i.begin();
+
+#if 0
+  ECA_LOG_MSG(ECA_LOGGER::system_objects, 
+	      "In tone read_buffer().");
+#endif
+
+  while(!i.end()) {
+    for(int n = 0; n < channels(); n++) {
+      if (i.end()) 
+	break;
+
+      *(i.current(n)) 
+	= g_pfSineTable[m_lPhase >> SINE_TABLE_SHIFT];
+
+    }
+
+    m_lPhase += m_lPhaseStep;
+
+    i.next();
+  }
+
+  change_position_in_samples(sbuf->length_in_samples());
 }
 
 void AUDIO_IO_TONE::write_buffer(SAMPLE_BUFFER* sbuf)
 {
   /* NOP */
+  DBC_CHECK(false);
 }
 
 void AUDIO_IO_TONE::seek_position(void)
 {
-  /* note: must be implemented! */
+  /* note: phase must be correct after arbitrary seeks */
+  m_lPhase = m_lPhaseStep * position_in_samples();
+
+  if (ECA_AUDIO_POSITION::length_set() == true &&
+      position_in_samples() <
+      ECA_AUDIO_POSITION::length_in_samples())
+    finished_rep = false;
+}
+
+void AUDIO_IO_TONE::setPhaseStepFromFrequency(const SAMPLE_SPECS::sample_t fFrequency, bool force)
+{
+  if (fFrequency != m_fCachedFrequency || force == true) {
+    if (fFrequency >= 0 && fFrequency < m_fLimitFrequency) 
+      m_lPhaseStep = (unsigned long)(m_fPhaseStepScalar * fFrequency);
+    else 
+      m_lPhaseStep = 0;
+    m_fCachedFrequency = fFrequency;
+  }
+}
+
+void AUDIO_IO_TONE::set_parameter(int param, 
+				  string value)
+{
+  switch (param)
+    {
+    case 1: 
+      {
+	AUDIO_IO::set_parameter (param, value);
+	break;
+      }
+    case 2:
+      {
+	/* type; only "sine" supported */
+	break;
+      }
+    case 3: 
+      {
+	setPhaseStepFromFrequency (atof(value.c_str()), false);
+	break;
+      }
+    case 4:
+      {
+	double duration = atof(value.c_str());
+	if (duration > 0.0f)
+	  ECA_AUDIO_POSITION::set_length_in_seconds(duration);
+	break;
+      }
+    }
+}
+
+string AUDIO_IO_TONE::get_parameter(int param) const
+{
+  switch (param) 
+    {
+    case 1: return AUDIO_IO::get_parameter(param);
+    case 2: return "sine";
+    case 3: return kvu_numtostr(m_fCachedFrequency);
+    case 4: 
+      {
+	if (ECA_AUDIO_POSITION::length_set() == true)
+	  return kvu_numtostr(ECA_AUDIO_POSITION::length_in_seconds_exact());
+	else
+	  return kvu_numtostr(-1.0f);
+      }
+    default: break;
+    }
+
+  return std::string();
 }
--- libecasound/audioio-tone.h
+++ libecasound/audioio-tone.h
@@ -4,6 +4,8 @@
 #include <string>
 #include "audioio.h"
 #include "samplebuffer.h"
+#include "samplebuffer_iterators.h"
+#include "sample-specs.h"
 #include "eca-audio-time.h"
 
 /**
@@ -59,9 +61,9 @@ class AUDIO_IO_TONE : public AUDIO_IO {
 
   virtual bool locked_audio_format(void) const { return false; }
   virtual bool supports_seeking(void) const { return true; }
-  virtual bool finite_length_stream(void) const { return true; }
+  virtual bool finite_length_stream(void) const;
 
-  virtual bool finished(void) const { return false; }
+  virtual bool finished(void) const { return finished_rep; }
 
   virtual void read_buffer(SAMPLE_BUFFER* sbuf);
   virtual void write_buffer(SAMPLE_BUFFER* sbuf);
@@ -72,18 +74,32 @@ class AUDIO_IO_TONE : public AUDIO_IO {
   virtual void set_buffersize(long int samples) { buffersize_rep = samples; }
   virtual long int buffersize(void) const { return buffersize_rep; }
 
+  virtual std::string parameter_names(void) const { return("tone,type,frequency,duration"); }
+  virtual void set_parameter(int param, string value);
+  virtual string get_parameter(int param) const;
+
   /*@}*/
 
-  /** @name New functions */
-  /*@{*/
-
-    
-  /*@}*/
 
 private:
 
   SAMPLE_BUFFER buffer_rep;
-  long int buffersize_rep;  
+  SAMPLE_ITERATOR_INTERLEAVED i;
+  long int buffersize_rep;
+  bool finished_rep;
+
+  unsigned long     m_lPhase;
+  unsigned long     m_lPhaseStep;
+  SAMPLE_SPECS::sample_t m_fCachedFrequency;
+  SAMPLE_SPECS::sample_t m_fLimitFrequency;
+  SAMPLE_SPECS::sample_t m_fPhaseStepScalar;
+
+  /** @name New functions */
+  /*@{*/
+   
+  void setPhaseStepFromFrequency(const SAMPLE_SPECS::sample_t fFrequency, bool force);
+
+  /*@}*/
 
   AUDIO_IO_TONE& operator=(const AUDIO_IO_TONE& x) { return *this; }
   AUDIO_IO_TONE (const AUDIO_IO_TONE& x) { }
