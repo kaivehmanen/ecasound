4d29762c1e8785f141ed5dfd8380bb737f23ff17
Kai Vehmanen <kvehmanen -at- eca -dot- cx>
2009-02-02 00:59:10 +0200

    Update ecasound copyright statement to year 2009.

--- ecasound/ecasound.h
+++ ecasound/ecasound.h
@@ -18,7 +18,7 @@ class ECA_SESSION;
  */
 
 #define ECASOUND_BANNER_ASTERISK_BAR "********************************************************************************\n"
-#define ECASOUND_COPYRIGHT           " (C) 1997-2008 Kai Vehmanen and others    "
+#define ECASOUND_COPYRIGHT           " (C) 1997-2009 Kai Vehmanen and others    "
 
 #define ECASOUND_RETVAL_SUCCESS         0    /**< Succesful run */
 #define ECASOUND_RETVAL_INIT_FAILURE    1    /**< Unable to initialize */
