6d713b6e5eda01872e01ad17c66644ccfcf0c7fc
Kai Vehmanen <kvehmanen -at- eca -dot- cx>
2009-03-22 12:27:14 +0200

    Updates to kvu_dbc.h implementation
    The formatting of warnings, emitted when design-by-contract
    assertions fail, has been slightly updated and the pretty
    printing is now implemented separately in kvu_dbc.cpp (versus
    in the header as before).
    
    As an additional feature, if ecasound is compiled with
    debug features enabled ('--enable-debug' to configure),
    a function stack backtrace is printed out whenever a DBC
    assertion fails. Currently this feature is only available
    with glibc (2.1 and newer).

--- kvutils/kvu_dbc.h
+++ kvutils/kvu_dbc.h
@@ -1,6 +1,6 @@
 /**************************************************************************
- * dbc.h: Design-by-contract framework for C and C++
- * Copyright (C) 2001,2002 Kai Vehmanen
+ * dbc.h: A barebones design-by-contract framework for C and C++
+ * Copyright (C) 2001,2002,2009 Kai Vehmanen
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,25 +20,54 @@
 #ifndef INCLUDED_DBC_H
 #define INCLUDED_DBC_H
 
-#define DBC_NEVER_REACHED(x) DBC_CHECK(false)
-
 #ifdef ENABLE_DBC
 
 #include <stdio.h>
 
-#define DBC_REQUIRE(expr)						      \
-   (expr) ? (void)(0) :	(void)(fprintf(stderr, "Warning: DBC_REQUIRE failed - \"%s\", %s, %d.\n", #expr,__FILE__, __LINE__))
-#define DBC_ENSURE(expr)							      \
-   (expr) ? (void)(0) :	(void)(fprintf(stderr, "Warning: DBC_ENSURE failed - \"%s\", %s, %d.\n", #expr,__FILE__, __LINE__))
-#define DBC_CHECK(expr)							      \
-   (expr) ? (void)(0) :	(void)(fprintf(stderr, "Warning: DBC_CHECK failed - \"%s\", %s, %d.\n", #expr,__FILE__, __LINE__))
+void kvu_dbc_report_failure(const char *action, const char* expr, const char* file, const char* func, int lineno);
+
+/**
+ * Declare a boolean precondition assertion. A warning is emitted
+ * if expr is false.
+ */
+#define DBC_REQUIRE(expr) \
+   (expr) ? (void)(0) :	kvu_dbc_report_failure("DBC_REQUIRE", #expr, __FILE__, __func__,  __LINE__)
+
+/**
+ * Declare a boolean postcondition assertion. A warning is emitted
+ * if expr is false.
+ */
+#define DBC_ENSURE(expr) \
+   (expr) ? (void)(0) :	kvu_dbc_report_failure("DBC_ENSURE", #expr, __FILE__, __func__,  __LINE__)
+
+/**
+ * Declare a boolean invariant assertion. A warning is emitted
+ * if expr is false.
+ */
+#define DBC_CHECK(expr)	\
+   (expr) ? (void)(0) :	kvu_dbc_report_failure("DBC_CHECK", #expr, __FILE__, __func__,  __LINE__)
+
+/**
+ * Emits warning if macro is ever executed.
+ */
+#define DBC_NEVER_REACHED(expr) \
+   kvu_dbc_report_failure("DBC_NEVER_REACHED", NULL, __FILE__, __func__,  __LINE__)
+
+/**
+ * A helper macro for declaring variables, etc that will be
+ * only defined when ENABLE_DBC is enabled.
+ */
 #define DBC_DECLARE(expr)               expr
 
 #else /* DBC DISABLED --> */
 
+/* When ENABLE_DBC is not defined, the macros expand to no-op
+   statements */
+
 #define DBC_REQUIRE(expr)		((void) 0)
 #define DBC_ENSURE(expr)		((void) 0)
 #define DBC_CHECK(expr)		        ((void) 0)
+#define DBC_NEVER_REACHED(x)            ((void) 0)
 #define DBC_DECLARE(expr)               ((void) 0)
 
 #endif /* <-- DBC DISABLED */
