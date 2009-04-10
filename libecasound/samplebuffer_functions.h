440dd13d9647cde8191dbf9a368d4122b9a2a2e4
Kai Vehmanen <kvehmanen -at- eca -dot- cx>
2009-04-10 12:54:55 +0300

    Added new helper functions to SAMPLE_BUFFER_FUNCTIONS

--- libecasound/samplebuffer_functions.h
+++ libecasound/samplebuffer_functions.h
@@ -1,3 +1,25 @@
+// ------------------------------------------------------------------------
+// samplebuffer_functions.cpp: Extra functions for SAMPLE_BUFFER class
+// Copyright (C) 2000,2001,2009 Kai Vehmanen
+//
+// Attributes:
+//     eca-style-version: 3
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+// ------------------------------------------------------------------------
+
 #ifndef INCLUDED_SAMPLEBUFFER_FUNCTIONS_H
 #define INCLUDED_SAMPLEBUFFER_FUNCTIONS_H
 
@@ -75,6 +97,10 @@ class SAMPLE_BUFFER_FUNCTIONS {
     }
     return(sqrt(temp_avg / count_samples));
   }
+
+  static void fill_with_random_samples(SAMPLE_BUFFER *sbuf);
+  static bool is_almost_equal(const SAMPLE_BUFFER& a, const SAMPLE_BUFFER& b);
+
 };
 
 #endif
