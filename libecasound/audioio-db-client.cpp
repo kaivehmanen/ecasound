kaivehmanen/ecasound

commit 2f0d066a49cf8ce2d681df83de6ed2a47155a148

Make sure a warning is printed if a seek operation is sent through to double-buffered i/o system to an object that does not support seeking.

libecasound/audioio-db-client.cpp
@@ -254,7 +254,7 @@ SAMPLE_SPECS::sample_pos_t AUDIO_IO_DB_CLIENT::seek_position(SAMPLE_SPECS::sampl
     restore_db_server_state(was_running);
   }
 
-  return res;
+  return AUDIO_IO_PROXY::seek_position(res);
 }
 
 /**

commit 4ab1745998e7eab3f1f36c0f1da37d21fb4578ff

Fix bug with double-buffered files and live seeks
In case a seek is performed while server for double-buffered i/o
is running, an erronous additional seek is executed via AUDIO_IO_PROXY
parent class. This in practise caused the server to loop the first
segment of the stream after every live seek.

This affects especially live seeks used when ecasound is following
JACK transport state.

This bug was introduced in commit 2f0d066a49cf8ce2d681df83de6ed2a47155a148
and in 2.6.0 release.

libecasound/audioio-db-client.cpp
@@ -2,7 +2,7 @@
 // audioio-db-client.cpp: Client class for double-buffering providing 
 //                        additional layer of buffering for objects
 //                        derived from AUDIO_IO.
-// Copyright (C) 2000-2005,2009 Kai Vehmanen
+// Copyright (C) 2000-2005,2009,2011 Kai Vehmanen
 //
 // Attributes:
 //     eca-style-version: 3
@@ -258,7 +258,9 @@ SAMPLE_SPECS::sample_pos_t AUDIO_IO_DB_CLIENT::seek_position(SAMPLE_SPECS::sampl
     restore_db_server_state(was_running);
   }
 
-  return AUDIO_IO_PROXY::seek_position(res);
+  /* note: important that we override the AUDIO_IO_PROXY
+   *       default implementation as it does the wrong thing */
+  return AUDIO_IO::seek_position(res);
 }
 
 /**